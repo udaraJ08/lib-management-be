72863141c7d568fc5c65eb059c255140566907a8
Udara J(SiniSTeR) <67888873+udaraJ08@users.noreply.github.com>
2024-04-06 11:59:30 +0530

    role entity created

--- src/role/entities/role.entity.ts
+++ src/role/entities/role.entity.ts
@@ -1,1 +1,32 @@
-export class Role {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('role')
+export class RoleEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({
+    type: 'varchar',
+    length: 10,
+    nullable: false,
+  })
+  name: string;
+
+  @Column({
+    type: 'varchar',
+    nullable: false,
+  })
+  description: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
