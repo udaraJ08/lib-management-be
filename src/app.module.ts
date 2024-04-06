f573cf221a715a50e06834b76bd462f14c4dc6da
Udara J(SiniSTeR) <67888873+udaraJ08@users.noreply.github.com>
2024-04-06 09:22:58 +0530

    book entity created

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,30 @@ import { BorrowModule } from './borrow/borrow.module';
 import { BookModule } from './book/book.module';
 import { AuthorModule } from './author/author.module';
 import { PaymentModule } from './payment/payment.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BookEntity } from './book/entities/book.entity';
 
 @Module({
-  imports: [UserModule, AuthModule, RoleModule, BorrowModule, BookModule, AuthorModule, PaymentModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_DEV_HOST,
+      port: Number.parseInt(process.env.DB_PORT),
+      password: 'root',
+      username: 'postgres',
+      entities: [BookEntity],
+      database: 'lib_management_db',
+      synchronize: true,
+      logging: true,
+    }),
+    UserModule,
+    AuthModule,
+    RoleModule,
+    BorrowModule,
+    BookModule,
+    AuthorModule,
+    PaymentModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/book/entities/book.entity.ts
+++ src/book/entities/book.entity.ts
@@ -1,1 +1,27 @@
-export class Book {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class BookEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({
+    type: 'varchar',
+    length: 30,
+    nullable: false,
+  })
+  name: string;
+
+  @Column({
+    type: 'varchar',
+    length: 30,
+    nullable: false,
+  })
+  address: string;
+
+  @Column({
+    type: 'date',
+    nullable: false,
+  })
+  dob: string;
+}
